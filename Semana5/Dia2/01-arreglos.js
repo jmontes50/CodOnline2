3e40c7cde8308fed189b9e7dded2730056f3f9dd
Osmar Montesinos <osmarmontes50@gmail.com>
2021-01-14 18:52:52 -0500

    update

---
+++ Semana5/Dia2/01-arreglos.js
@@ -0,0 +1,38 @@
+var nom1 = "Javier";
+var nom2 = "Daysi";
+var nom3 = "Sandra";
+var nom4 = "David";
+
+// Arreglos, Array
+var alumnos = ["Javier","Daysi","Sandra","David","Carlos"];
+
+console.log(alumnos);
+
+console.log(alumnos[3]);
+
+console.log(alumnos[1]);
+
+//length es la longitud o la cantidad de elementos que tengo en mi arreglito
+console.log(alumnos.length);
+
+var platillos = ["Caldo Blanco", "Kam Lu Wantan", "Lomo Saltado", "Seco de Chavelo", "Ceviche", "Sopa Seca", "Arroz con Pato", "Puca Picante"];
+
+console.log(platillos.length)
+
+console.log(platillos[4]);
+
+var arreglo = ["cuy",8,true];
+
+var primos = [1,2,3,5,7,11];
+
+var vacio = [];
+
+console.log(vacio);
+
+console.log(typeof arreglo[0]);
+
+var matriz = [[0,1,5],[10,5,1,6,10,20],[-1,0,9]];
+
+console.log(matriz.length);
+
+console.log(matriz[1].length);
