1d86e63ca6e5a622dd5821e100b8ba333a792ecb
Osmar Montesinos <osmarmontes50@gmail.com>
2021-01-21 19:50:17 -0500

    update

--- Semana6/Dia2/02-guardarObjeto.js
+++ Semana6/Dia2/02-guardarObjeto.js
@@ -1,4 +1,4 @@
-//LocalStorage, solo guarda texto
+//LocalStorage, solo guarda texto!!!!!! IMPORTANTE
 
 //1. Obtenemos los elementos que necesitamos
 var formulario = document.getElementById("registro")
@@ -10,13 +10,34 @@ var inputEdad = document.getElementById("edad");
 formulario.addEventListener("submit", function(evento){
   //En el caso de un ev submit, tengo que hacer un preventDefault para que no se recargue la pestaña
   evento.preventDefault();
-
+  //obtenemos los valores de los input
   var objUsuario = {
     nombre: inputNombre.value,
     apellidos: inputApellido.value,
     edad: inputEdad.value
   }
   //imprimo el objeto
-  console.log(objUsuario)
+  // console.log(objUsuario)
+
+  //3. me toca guardar los datos, pero para guardarlos voy a tener que convertirlos a texto
+  //JSON, es ub objeto que existe dentro de JS
+  var objATexto = JSON.stringify(objUsuario)
+  // console.log(objATexto)
+
+  //4. Como ya esta en Texto, lo guardo en el LocalStorage
+  localStorage.setItem("usuario", objATexto)
+})
+
+//-------------------------------------------------
+
+var btnVerUsuario = document.getElementById("verusuario");
+
+btnVerUsuario.addEventListener("click", function(){
+  var usuarioObtenido = localStorage.getItem("usuario");
+  // console.log(usuarioObtenido)
+  //Transformar texto a JS 
+  //JSON.parse convierte texto a una estructura de Javascript
+  var TextoAJavascript = JSON.parse(usuarioObtenido);
+  console.log(TextoAJavascript);
 })
 
