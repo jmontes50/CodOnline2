3dfc0d174884f98e4229e1764a2719c34e9770fe
Osmar Montesinos <osmarmontes50@gmail.com>
2021-01-21 21:30:50 -0500

    XHR

---
+++ Semana6/Dia2/03-ajax.js
@@ -0,0 +1,41 @@
+var btndatos = document.getElementById("boton");
+
+btndatos.addEventListener("click", function(){
+
+  //vamos a configurar nuestro asistente que va a enviar la petición y va a esperar su respuesta
+
+  var asistente = new XMLHttpRequest();
+
+  //este asistente va a tener sus propios eventos
+  asistente.addEventListener("readystatechange", function(){
+
+    //readyState, me va a indicar cuál es estado de mi petición, va del 1 al 4
+    switch(asistente.readyState){
+      case 1:
+        console.log("OPEN", asistente.readyState)//conf
+        break;
+      case 2:
+        console.log("SEND", asistente.readyState)//enviando
+        break;
+      case 3:
+        console.log("DATA, esperando", asistente.readyState);
+        break;
+      case 4:
+        console.log("YA RECIBI LA RPTA", asistente.readyState) //El que me interesa
+        //.responseText es una propiedad donde se guarda mi respuesta de la petición.
+        var objRespuesta = JSON.parse(asistente.responseText)
+        console.log(objRespuesta)
+        break;
+      default:
+        console.log("Sucedió un error");
+        break;
+    }
+  })
+  //aca finaliza el readystatechange
+
+  //open hace la configuración del envio, a donde y como
+  asistente.open("GET", "https://reqres.in/api/users?page=2")
+
+  //send, cuando ya enviamos la petición a la API
+  asistente.send(null);
+})
