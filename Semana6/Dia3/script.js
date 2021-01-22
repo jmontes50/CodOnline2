a927b9e42f479fa04047ab6fdbea458ee458af1e
Osmar Montesinos <osmarmontes50@gmail.com>
2021-01-22 18:56:42 -0500

    update

---
+++ Semana6/Dia3/script.js
@@ -0,0 +1,39 @@
+//platillos
+
+var platillos = [
+  {
+    nombre:"Caldo Blanco",
+    descripcion:"Caldo cocinado a partir de Apio, Cordero y Gallina, tradicional de Arequipa",
+    precio: 15.00,
+    stock: 20,
+    imagen:"https://cde.peru.com//ima/0/1/8/4/2/1842580/380x300/arequipa.jpg"
+  },
+  {
+    nombre:"triple",
+    descripcion:"Rocoto Relleno, Pastel de Papa, Chicharron de Chancho",
+    precio:40.00,
+    stock:10,
+    imagen:"https://1.bp.blogspot.com/-ehPgUGaU3Ao/UfcO-h2Ev4I/AAAAAAAADz0/Ih-zVYWyo78/s1600/DSC00434.jpg"
+  },
+  {
+    nombre:"Escribano",
+    descripcion:"Plato de entrada bastante picante a base de papa, rocoto, habas, tomate y aliño",
+    precio:10.00,
+    stock:15,
+    imagen:"https://miqueridaarequipa.info/wp-content/uploads/2018/08/Escribano-plato-juridico-de-Arequipa.jpg"
+  },
+  {
+    nombre:"Ceviche",
+    descripcion:"Pescado cocinado a base de limón, plato tradicional del Perú",
+    precio:25.00,
+    stock:20,
+    imagen:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Cebiche_de_corvina.JPG/1200px-Cebiche_de_corvina.JPG"
+  },
+  {
+    nombre:"Picante Cuy",
+    descripcion:"Cuy, aji dorado, aderezado, acompañado de papas",
+    precio:40.00,
+    stock:6,
+    imagen:"https://jameaperu.com/wp-content/uploads/2020/03/picante-de-cuy_700x464.jpg"
+  }
+]
