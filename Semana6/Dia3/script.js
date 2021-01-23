95b6736a14939e9c2a6ed9add62a04d7c2f992e4
Osmar Montesinos <osmarmontes50@gmail.com>
2021-01-22 20:44:41 -0500

    update, descripcion del programita

--- Semana6/Dia3/script.js
+++ Semana6/Dia3/script.js
@@ -1,7 +1,9 @@
 //platillos
-
+//1. El arreglo de objetos de donde vamos a obtener los platillos
+//arreglo, API, todos tienen la misma estructura (propiedades)
 var platillos = [
   {
+    id:1,
     nombre:"Caldo Blanco",
     descripcion:"Caldo cocinado a partir de Apio, Cordero y Gallina, tradicional de Arequipa",
     precio: 15.00,
@@ -9,6 +11,7 @@ var platillos = [
     imagen:"https://cde.peru.com//ima/0/1/8/4/2/1842580/380x300/arequipa.jpg"
   },
   {
+    id:2,
     nombre:"triple",
     descripcion:"Rocoto Relleno, Pastel de Papa, Chicharron de Chancho",
     precio:40.00,
@@ -16,6 +19,7 @@ var platillos = [
     imagen:"https://1.bp.blogspot.com/-ehPgUGaU3Ao/UfcO-h2Ev4I/AAAAAAAADz0/Ih-zVYWyo78/s1600/DSC00434.jpg"
   },
   {
+    id:3,
     nombre:"Escribano",
     descripcion:"Plato de entrada bastante picante a base de papa, rocoto, habas, tomate y aliño",
     precio:10.00,
@@ -23,6 +27,7 @@ var platillos = [
     imagen:"https://miqueridaarequipa.info/wp-content/uploads/2018/08/Escribano-plato-juridico-de-Arequipa.jpg"
   },
   {
+    id:4,
     nombre:"Ceviche",
     descripcion:"Pescado cocinado a base de limón, plato tradicional del Perú",
     precio:25.00,
@@ -30,6 +35,7 @@ var platillos = [
     imagen:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Cebiche_de_corvina.JPG/1200px-Cebiche_de_corvina.JPG"
   },
   {
+    id:5,
     nombre:"Picante Cuy",
     descripcion:"Cuy, aji dorado, aderezado, acompañado de papas",
     precio:40.00,
@@ -37,14 +43,33 @@ var platillos = [
     imagen:"https://jameaperu.com/wp-content/uploads/2020/03/picante-de-cuy_700x464.jpg"
   }
 ]
-//se va encargar de dibujar mis platillos en el HTML
-var divplatillos = document.getElementById("divplatillos");
 
+function obtenerBotones() {
+  var botonesPlatillos = document.getElementsByClassName("anadir");
+  // console.log(botonesPlatillos)
+  for(var j = 0; j < botonesPlatillos.length; j++){
+    botonesPlatillos[j].addEventListener("click", function(){
+      var idPlatillo = botonesPlatillos[j].getAttribute("id-platillo");
+      console.log(idPlatillo);
+    })
+  }
+  
+}
+
+
+
+//2. se va encargar de dibujar mis platillos en el HTML
+//A partir de mi arreglo de platillos, necesito convertirlos a algo que el navegador pueda interpretar: objeto -> HTML
+//2.1 Obtener donde voy a poner mis platillos en el HTML
+var divplatillos = document.getElementById("divplatillos");
+//2.2 Yo necesito un lugar donde guardar mis objetos platillos ya convertidos a HTML
 var tarjetasPlatillos = "";
 
+//2.3 Creo una función que recorra (con un for) mi arreglo de platillos y voy a obtener sus propiedades uno por uno
 function imprimirPlatillos() {
    for(var i = 0; i < platillos.length; i++){
-     //template string
+     //template string - https://carlosazaustre.es/ecmascript6
+     //2.4 tarjetasPlatillos es un string, y lo que hago es cada vez que pase por un objeto de mi arreglo de platillos, voy a crear en forma de texto un <div> que contenga la información de mi platillo
      tarjetasPlatillos = tarjetasPlatillos + 
      `<div class="tarjetaplatillo">
         <h4>${platillos[i].nombre}</h4>
@@ -55,10 +80,14 @@ function imprimirPlatillos() {
         <br>
         <label class="etiquetaplatillo">Stock</label>
         <span>${platillos[i].stock}</span>
+        <button class="anadir" id-platillo="${platillos[i].id}">
+          Añadir al Carrito
+        </button>
       </div>`
+      //**NT: al boton le he añadido una clase y una tributo id-platillo, la clase me servira para obtener todos los botones, después y poderles añadir un addEventListener y el atributo para poder obtener el id de cada platillo */
    }
-
+   //2.5 añadirlo todo ese texto al contenido de divplatillos, esto los hacemos con innerHTML
    divplatillos.innerHTML = tarjetasPlatillos;
+   obtenerBotones();
 }
-
 imprimirPlatillos();
