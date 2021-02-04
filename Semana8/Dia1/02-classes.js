8f7715b9efd7f022bc67cad289e9694dee75c204
Osmar Montesinos <osmarmontes50@gmail.com>
2021-02-03 21:18:56 -0500

    update class

--- Semana8/Dia1/02-classes.js
+++ Semana8/Dia1/02-classes.js
@@ -11,16 +11,39 @@ class Gatete{
     this.colour = color
     this.weight = peso
   }
+
+  //un método es una función, una acción que puede realizar el objeto creado a partir de la función
+  //nombre_metodo(args){}
+  maullar(veces){
+    console.log(`miau miau miaaaaauw ${veces} veces`)
+  }
+
+  cumpleanios(){
+    this.age = this.age + 1
+  }
+
+  yomismo(){
+    console.log(this)
+  }
 }
 //creamos la variable 
 //al utilizar la palabra "new" lo que hago es crear una nueva Instancia de mi clase
 let Gato1 = new Gatete("Abby",7,"angora","blanco",2)
-
 console.log(Gato1.colour)
 
 let Gato2 = new Gatete("Colorina",4,"Carei","naranja",1.5)
-
 console.log(Gato2.name)
 
+//ejecutando el método maullar
+Gato2.maullar(3)
 
+Gato2.yomismo()
+
+Gato1.yomismo()
+
+// console.log(Gato1.age)
+
+// Gato1.cumpleanios()
+
+// console.log(Gato1.age)
 
---
+++ Semana8/Dia1/03-masclasses.js
@@ -0,0 +1,52 @@
+class Bodega {
+  //definición de propiedades (aunque no es obligatorio)
+  nombre
+  direccion
+  productos
+  //constructor(), el cosntructor se ejecuta apenas se contrusye mi objeto
+  constructor(name, address) {
+    //this va a hacer referencia al objeto creado a partir de la clase
+    this.nombre = name
+    this.direccion = address
+    //estoy diciendo que todas las bodegas que voy a crear
+    //no van a tener productos
+    this.productos = []
+    //un metodo, puede utilizar otro metodo
+    this.mostrarInfo()
+  }
+
+  mostrarInfo(){
+    console.log(this.nombre)
+  }
+  //encapsulamiento
+  agregarProducto(productoNuevo){
+    this.productos.push(productoNuevo);
+    console.log("Producto agregado existosamente")
+  }
+
+}
+
+let miBodega = new Bodega("De Don Pepé", "Av. Dolores")
+
+// miBodega.mostrarInfo()
+
+//Modificar directamente el objeto esta mal
+// console.log(miBodega.productos)
+// //estoy modificando directamente la propuiedad productos
+// miBodega.productos = ["Arroz","Pollo","Inca Kola"]
+
+// console.log(miBodega.productos)
+
+// //agregardueño
+// miBodega.duenio = "El Pepé"
+
+// console.log(miBodega)
+
+miBodega.agregarProducto("Arroz")
+miBodega.agregarProducto("Chocolates")
+
+console.table(miBodega)
+
+let otraBodega = new Bodega("Norka","Parque los palotes")
+
+console.table(otraBodega)
