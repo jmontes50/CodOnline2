0ac1eb5f812885ce72312ac4e6e7094dc7932529
Osmar Montesinos <osmarmontes50@gmail.com>
2021-02-06 21:54:54 -0500

    update

--- Semana8/proyectito/js/App.js
+++ Semana8/proyectito/js/App.js
@@ -1,6 +1,7 @@
 let network = new Network()
 let interface = new Interface()
 let btnModal = document.getElementById("btnAgregarModal")
+let btnGuardar = document.getElementById("btnGuardar")
 
 let getProductos = () => {
   //1. se ejecuta Network con obtener Productos y me devuelve el arreglo de Objetos Producto de mockAPI
@@ -20,4 +21,27 @@ let modal = new bootstrap.Modal(document.getElementById("modalProducto1"))
 //Abriendo el modal
 btnModal.addEventListener("click", ()=>{
   modal.show();
+})
+
+//mediante ese btnGuardar voy a utilizar crearProducto de network para guardar un nuevo Producto
+btnGuardar.addEventListener("click",()=>{
+  //voy a obtener el formulario del HTML
+  let formulario = document.getElementById("formcrearproducto")
+  //obtengo el value por el name --> formulario["name"].value
+  let nuevoProducto = new Producto(formulario["producto_nombre"].value ,
+                                    formulario["producto_descripcion"].value ,
+                                    formulario["producto_precio"].value ,
+                                    formulario["producto_imagen"].value)
+
+  network.crearProducto(nuevoProducto)
+  .then(productoCreado => {
+    // console.log(productoCreado)
+    //reset me limpia los input del formulario
+    formulario.reset()
+    //CERRAMOS EL MODAL
+    modal.hide()
+    //Despuies que creamos un nuevo producto, obtenemos los productos actualizados y los imprimo de nuevo en el HTML
+    getProductos()
+  })
+  .catch(error => console.log(error))
 })
