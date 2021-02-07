8adb350adeda4b9484cbc4356794bd2430c9d55f
Osmar Montesinos <osmarmontes50@gmail.com>
2021-02-06 20:54:10 -0500

    update

--- Semana8/proyectito/js/App.js
+++ Semana8/proyectito/js/App.js
@@ -2,9 +2,12 @@ let network = new Network()
 let interface = new Interface()
 
 let getProductos = () => {
+  //1. se ejecuta Network con obtener Productos y me devuelve el arreglo de Objetos Producto de mockAPI
   network.obtenerProductos()
-  .then(productos => {
-    console.table(productos)
+  .then(productos => { //aca tengo el arreglo de Productos
+    // console.table(productos) 
+    //despues ese arreglo de productos, se lo paso a imprimir Productos
+    interface.imprimirProductos(productos)
   })
   .catch(error => console.log(error))
 }
--- Semana8/proyectito/js/Interface.js
+++ Semana8/proyectito/js/Interface.js
@@ -4,10 +4,38 @@ class Interface {
   imprimirProductos(arregloProductos){ //si esta clase va a poner algo en el DOM, tendrá que sacarlo de algun lado
     const contenido = document.getElementById("contenido")
     //estadoContenido va a guardar el contenido
+    //opcion1
     let estadoContenido = arregloProductos.map((producto)=>{
-      return producto
+      //Aquí a partir de c/obj que es producto
+      //voy a crear un elemento para poner en el HTML
+      let cardProducto = 
+      `<div class="col-lg-3 col-md-6 col-sm-12 mb-2">
+        <div class="card">
+          <img class="card-img-top" src="${producto.producto_imagen}">
+          <div class="card-body">
+            <h5 class="card-title">
+              ${producto.producto_nombre}
+              <p class="card-text">
+                ${producto.producto_descripcion}
+              </p>
+              <p class="card-text">
+                Precio: ${producto.producto_precio}
+              </p>
+            </h5>
+          </div>
+        </div>
+      </div>`
+      return cardProducto
     })
-    
-    console.log(estadoContenido)
+    //Como es un arreglo de Strings, utilizo join para juntarlo todo en una sola variable
+    let htmlContenido = estadoContenido.join('')
+    //como ya tenemos los cards en HTML con la info, lo pasamos a "contenido"
+    contenido.innerHTML = htmlContenido
+
+    //opcion2, con desestructuración
+    // let estadoContenido = arregloProductos.map(({producto_id, producto_nombre,producto_descripcion, producto_imagen})=>{
+    //   return producto_nombre
+    // })
   }
+
 }
