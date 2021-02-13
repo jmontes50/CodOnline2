a1fa8da816c95cc87d95cc5d0069d722aa12963c
Osmar Montesinos <osmarmontes50@gmail.com>
2021-02-12 19:56:47 -0500

    update

--- Semana9/todo/src/App.js
+++ Semana9/todo/src/App.js
@@ -2,18 +2,27 @@
 //useState es el que se va a encargar de manejar el estado
 //en un componente funcional
 import {useState} from 'react'
+import Footer from './components/Footer'
 
 export default function App(){
   //let [estado, funcionActualizaEstado] = useState(EstadoInicial)
   let [tarea, setTarea] = useState("escribir")
   //puedo tener cuantos estados quiera
   let [lista, setLista] = useState([])
+
   let titulo = "To-do App"
+
+  let mensaje = "Creado por Tecsup"
 
   let agregarTarea = () =>{
     //esta funcion se va a encargar de agregar tarea al estado de mi lista
     //...lista <- desestructurando y agrego tarea
+    //spread-operator - esparcir
     setLista([...lista, tarea])
+  }
+
+  let mostrarDerechos = () => {
+    alert("Derechos Reservados Tecsup 2021")
   }
 
   //componentes controlados
@@ -37,6 +46,12 @@ export default function App(){
       {lista.map((item)=>(
         <p>{item}</p>
       ))}
+      {/* footer con props*/}
+      <Footer 
+        miMensaje={mensaje} 
+        miNumero="1234" 
+        miFuncion={mostrarDerechos}
+      />
     </div>
   )
 }
--- Semana9/todo/src/components/Footer.js
+++ Semana9/todo/src/components/Footer.js
@@ -1,10 +1,15 @@
 import {useState} from "react"
 
-export default function Footer () {
+export default function Footer ({miMensaje, miNumero, miFuncion}) {
   return(
     <div>
       <hr/>
       <h4>Footer</h4>
+      <h5>{miMensaje}</h5>
+      <p>{miNumero}</p>
+      <button onClick={()=>{miFuncion()}}>
+        Mostrar Info
+      </button>
     </div>
   )
 }
