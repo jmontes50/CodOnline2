241522181586502cf877f02531efdff1a03e0818
Osmar Montesinos <osmarmontes50@gmail.com>
2021-02-18 18:28:14 -0500

    update

--- Semana9/todo-list/src/components/InputComponent.js
+++ Semana9/todo-list/src/components/InputComponent.js
@@ -4,6 +4,20 @@ export default function InputComponent({listaTareas, actualizarLista}) {
 
   //const [state, setState] = useState(estado inicial)
   const [tareaNueva, setTareaNueva] = useState("")
+  const [estadoSelect, setEstadoSelect] = useState("")
+
+  const agregarTarea = () => {
+    actualizarLista([...listaTareas, tareaNueva])
+    setTareaNueva("")
+  }
+
+  const agregarTareaEnter = (e) => {
+    if(e.key === "Enter"){
+      agregarTarea()
+    }else{
+      return
+    }
+  }
 
   return (
     <div>
@@ -12,10 +26,16 @@ export default function InputComponent({listaTareas, actualizarLista}) {
         placeholder="Agregar Tarea"
         value={tareaNueva}
         onChange={(e)=>{setTareaNueva(e.target.value)}}
+        onKeyUp={(e)=>{agregarTareaEnter(e)}}
       />
-      <button onClick={() => {actualizarLista([...listaTareas, tareaNueva])}}>
+      <button onClick={() => {agregarTarea()}}>
         Agregar
       </button>
+
+      <select value={estadoSelect} onChange={(e)=>{setEstadoSelect(e.target.value)}}>
+        <option value="Lima">Lima</option>
+        <option value="Arequipa">Arequipa</option>
+      </select>
     </div>
   )
 }
