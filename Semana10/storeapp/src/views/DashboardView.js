d783a3c6db0255e011c9ba7eb03f27d9ad99c6a1
Osmar Montesinos <osmarmontes50@gmail.com>
2021-02-24 19:39:26 -0500

    update

--- Semana10/storeapp/src/views/DashboardView.js
+++ Semana10/storeapp/src/views/DashboardView.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {obtenerProductos} from '../services/productoService'
 
 export default function DashboardView() {
+  const [productos, setProductos] = useState([])
+
+  const getProducts = async () => {
+    let response = await obtenerProductos()
+    setProductos(response)
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
   return (
     <div className="mt-3">
       <h1 className="display-4">
@@ -13,6 +25,29 @@ export default function DashboardView() {
               Crear Producto
             </button>
           </div>
+        </div>
+        <div className="col-12">
+          {/* Mostrando Productos */}
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Descripcion</th>
+                <th>Precio</th>
+                <th>Opciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {productos.map((prod, i) => (
+                <tr key={i}>
+                  <td>{prod.nombre}</td>
+                  <td>{prod.descripcion}</td>
+                  <td>{prod.precio}</td>
+                  <td></td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
