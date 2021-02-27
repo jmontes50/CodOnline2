64d1f2387645d52268ba34082a1760f08074a84f
Osmar Montesinos <osmarmontes50@gmail.com>
2021-02-26 20:11:48 -0500

    update

--- Semana10/storeapp/src/context/authFireContext.js
+++ Semana10/storeapp/src/context/authFireContext.js
@@ -1,16 +1,13 @@
 import {useState, createContext} from 'react'
-import {useHistory} from 'react-router-dom'
 
 export const AuthFireContext = createContext() //creo un contexto con este nombre
 
 const AuthContextProvider = (props) => {
   const [userId, setUserId] = useState(null) //estado global
 
-  let history = useHistory()
-
   const setAuthUserId = (id) => { //funcion que se encarga de actualizar el estado global
     setUserId(id) //actualizo el estado global
-    return history.push('/') //me lleva a la ruta base
+
   }
 
   return(
