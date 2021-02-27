573ccb9d95cea34a8f548a5c07145c241a22828a
Osmar Montesinos <osmarmontes50@gmail.com>
2021-02-27 15:36:59 -0500

    update

--- Semana10/storeapp/src/views/LoginView.js
+++ Semana10/storeapp/src/views/LoginView.js
@@ -1,6 +1,8 @@
 import React, {useState, useContext} from 'react' //useContext me permite utilizar un contexto dentro de mi componente
 import {loginFire} from "../services/authFireService"
 import {AuthFireContext} from '../context/authFireContext'//necesitamos saber que context
+import Swal from 'sweetalert2'
+import {useHistory} from 'react-router-dom'
 
 export default function LoginView() {
   const [email, setEmail] = useState('')
@@ -8,12 +10,23 @@ export default function LoginView() {
 
   const {userId, setAuthUserId} = useContext(AuthFireContext) //de esta manera utilizo el contexto dentro de mi componente
   
+  let history = useHistory()
+
   const login = (e) => {
     e.preventDefault()
     loginFire(email, password)
     .then(rpta => {
       console.log(rpta)
-      setAuthUserId(rpta.user.uid)
+      setAuthUserId(rpta.user.uid) //con esto cambiamos el estado userId del context
+      Swal.fire({ //con esta parte mostramos la alertita que diga usuario logueado
+        icon:'success',
+        title:'Login Exitoso!!!',
+        timer:2000,
+        showConfirmButton:false
+      })
+      .then(()=>{//derspues que se muestra mi alerta, redirijo al usuario hacia /dashboard
+        history.push('/dashboard')
+      })
     })
     .catch(error => console.log(error))
   }
