2182f4bacc657cda981d9bb767bc1dbbb8792aeb
Osmar Montesinos <osmarmontes50@gmail.com>
2021-03-04 19:40:05 -0500

    update

--- Semana10/storeapp/src/views/CheckoutView.js
+++ Semana10/storeapp/src/views/CheckoutView.js
@@ -1,13 +1,19 @@
-import React, {useContext, useState} from 'react'
-import {CarritoContext} from "../context/carritoContext"
+import React, { useContext, useState } from "react";
+import { CarritoContext } from "../context/carritoContext";
 
 export default function CheckoutView() {
+  const { carrito } = useContext(CarritoContext);
+  let carritoTmp = [...carrito];
+  let totalCarrito = carritoTmp.reduce(
+    (total, prod) => total.precio * total.cantidad + prod.precio * prod.cantidad
+  )
 
-  const {carrito} = useContext(CarritoContext)
-  
   return (
     <div>
-      
+      <h1 className="display-6">Finalizar Compra</h1>
+      <div className="alert alert-success">
+        Tu carrito actual tiene un valor actual de: S/ {totalCarrito}
+      </div>
     </div>
   )
 }
