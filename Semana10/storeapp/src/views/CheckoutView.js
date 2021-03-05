d230cd5b24fe6bcc6af98bd69451ce0dc84f1337
Osmar Montesinos <osmarmontes50@gmail.com>
2021-03-04 20:19:18 -0500

    update

--- Semana10/storeapp/src/views/CheckoutView.js
+++ Semana10/storeapp/src/views/CheckoutView.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from "react";
 import { CarritoContext } from "../context/carritoContext";
+import { useForm } from "react-hook-form"
 
 export default function CheckoutView() {
   const { carrito } = useContext(CarritoContext);
@@ -7,6 +8,15 @@ export default function CheckoutView() {
   let totalCarrito = carritoTmp.reduce(
     (total, prod) => total.precio * total.cantidad + prod.precio * prod.cantidad
   )
+    //trayendo las herramientas de hook forms
+    //register = pàra registrar mis input
+    //handleSUbmit es una funcion que maneja el onSubmit del form
+    //errors, verifica las validaciones de register y según eso muestra un error
+  let { register, handleSubmit, errors } = useForm()
+
+  let escucharSubmit = (data) => {
+    console.log(data)
+  }
 
   return (
     <div>
@@ -14,6 +24,18 @@ export default function CheckoutView() {
       <div className="alert alert-success">
         Tu carrito actual tiene un valor actual de: S/ {totalCarrito}
       </div>
+      <form onSubmit={handleSubmit(escucharSubmit)}>
+        <div>
+          <label className="form-label">Nombre Completo</label>
+          <input type="text" name="nombre" className="form-control" ref={register({required: true})} />
+          {errors.nombre && errors.nombre.type === "required" && (
+            <small className="text-danger">El nombre es obligatorio</small>
+          )}
+        </div>
+        <button type="submit" className="btn btn-success btn-lg mt-3">
+          Realizar Compra
+        </button>
+      </form>
     </div>
   )
 }
